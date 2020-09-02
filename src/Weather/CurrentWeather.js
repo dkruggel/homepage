838b1da790433873755b20a3ea98d4ce1340f169
David Kruggel <eng6@natoli.com>
2020-09-02 08:51:19 -0500

    Catch up to prev

--- src/Weather/CurrentWeather.js
+++ src/Weather/CurrentWeather.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 const openWeatherKey = '6102a812da477a662eca40c6b33cf325';
 // const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
@@ -12,6 +13,7 @@ export default class CurrentWeather extends React.Component {
       real_feel: 0,
       condition: '',
       icon: '',
+      daily: [],
     };
   }
 
@@ -45,83 +47,19 @@ export default class CurrentWeather extends React.Component {
         'https://openweathermap.org/img/wn/' +
         currentDay.current.weather[0].icon +
         '@2x.png',
+      daily: currentDay.daily,
     });
-    // return `<h2 id="weather-data">${this.buildDate()}</h2>
-    //           <div id="weather-conditions">
-    //           <h4 id="weather-data">Temperature: ${this.kelvinToFahrenheit(
-    //             currentDay.current.temp
-    //           )}&deg;F Condition: ${
-    //   currentDay.current.weather[0].description
-    // }</h4> <img id="weather-icon" src="https://openweathermap.org/img/wn/${
-    //   currentDay.current.weather[0].icon
-    // }@2x.png"></div>
-    //           <h4 id="weather-data">Real Feel: ${this.kelvinToFahrenheit(
-    //             currentDay.current.feels_like
-    //           )}&deg;F  Rain: ${(currentDay.current.rain['1h'] / 25.4).toFixed(
-    //   2
-    // )}&quot;</h4><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[new Date().getDay()]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[0].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[0].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 1) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[1].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[1].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 2) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[2].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[2].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 3) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[3].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[3].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 4) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[4].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[4].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 5) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[5].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[5].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 6) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[6].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[6].temp.min
-    // )}&deg;F</h4></div><div id="weather-conditions"><h4 id=weather-data>${
-    //   weekDays[(new Date().getDay() + 7) % 7]
-    // }: ${this.kelvinToFahrenheit(
-    //   currentDay.daily[7].temp.max
-    // )}&deg;F</h4><h4 id=weather-data>${this.kelvinToFahrenheit(
-    //   currentDay.daily[7].temp.min
-    // )}&deg;F</h4></div>`;
   };
 
   renderForecast = (day) => {
     return this.createWeatherHTML(day);
-    // const weatherContent = this.createWeatherHTML(day);
-    // document.getElementById('weather').innerHTML = weatherContent;
-    // document.getElementById('greet').innerHTML = setGreeting();
   };
 
   render() {
     return (
       <div
         style={{
-          fontSize: '1.5em',
+          fontSize: '1.75em',
           paddingLeft: '0.25em',
           paddingTop: '0.25em',
         }}
@@ -133,6 +71,31 @@ export default class CurrentWeather extends React.Component {
         <div style={{ display: 'flex', margin: '0.5em', alignItems: 'center' }}>
           Condition: {this.state.condition}{' '}
           <img id="weather-icon" src={this.state.icon} alt="weather icon" />
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '15.5em',
+          }}
+        >
+          {this.state.daily.map((day, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'flex-end',
+                  margin: '0.25em',
+                }}
+              >
+                <div>{moment().add(index, 'days').format('dddd')}: &nbsp;</div>
+                <div>{this.kelvinToFahrenheit(day.temp.max)}°F &ensp; </div>
+                <div>{this.kelvinToFahrenheit(day.temp.min)}°F</div>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
