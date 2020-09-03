ba4a0d3d7eda23de23e581eaabb09f1f722ecf4e
David Kruggel <eng6@natoli.com>
2020-09-03 08:42:03 -0500

    Add countdown

--- src/Countdown/Countdown.js
+++ src/Countdown/Countdown.js
@@ -3,17 +3,26 @@ import dates from './dates.json';
 import Moment from 'moment';
 
 export default class Countdown extends React.Component {
+  daysRemaining(eventDate) {
+    let e = Moment(eventDate);
+    return e.diff(Moment(), 'days');
+  }
+
   render() {
-    // let newDates = [];
-    dates.dates.map((event, index) => {
-      let difference = Moment(event.Date, 'MM-DD-YYYY').fromNow();
-      console.log(`${event.Event}: ${event.Date}  ${difference}`);
-      return (
-        <div>
-          {event.Event}: {event.Date}
-        </div>
-      );
-    });
-    return <div></div>;
+    return (
+      <div>
+        {dates.dates.map((event, index) => {
+          let difference = this.daysRemaining(event.Date);
+          //console.log(event.Event + ': ' + this.daysRemaining(event.Date));
+          return (
+            <div key={index}>
+              <h6>
+                {event.Event}: {event.Date} &nbsp; {difference} days
+              </h6>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
